1a91031e3b1ee4d7390c926a1fbd778b1671bd08
Ecksi <jimmy.dechant@gmail.com>
2018-04-04 08:20:10 -0600

    Add tests for Trie class Insert method

--- tests/Trie-test.js
+++ tests/Trie-test.js
@@ -9,19 +9,29 @@ describe('TRIE', () => {
     trie = new Trie();
   });
 
-  it('should start with zero elements', () => {
-    expect(trie.length).to.eq(0);
-  });
-
-  it('should set its default head to null', () => {
-    expect(trie.head).to.eq(null);
-  });
-
   describe('INSERT', () => {
     it('should add a word to the trie', () => {
       trie.insert('hitmonchan');
-      expect(trie.length).to.equal(1)
-      expect(trie.head.data).to.equal('hitmonchan')
+
+      expect(trie.count).to.equal(1);
+      expect(trie.root.children.h.data).to.equal('h');
+      expect(trie.root.children.h.children.i.data).to.equal('i');
+    })
+
+    it('should be able to add four words to the trie', () => {
+      trie.insert('hitmonchan');
+      trie.insert('hatmonlee');
+      trie.insert('mew');
+      trie.insert('bulbasaur');
+      trie.insert('diglet');
+
+      expect(trie.count).to.equal(5);
+      expect(Object.keys(trie.root.children.h.children).length).to.equal(2);
+      expect(trie.root.children.m.children.e.children.w.end).to.equal(true);
+      expect(trie.root.children.b.data).to.equal('b');
+      expect(trie.root.children.b.children.i).to.equal(undefined);
+      expect(trie.root.children.d.data).to.equal('d');
+      expect(trie.root.children.d.children.i.data).to.equal('i');
     })
   })
 })
