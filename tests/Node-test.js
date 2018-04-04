e712e912e5395ce574182d22f447753317a2f005
Ecksi <jimmy.dechant@gmail.com>
2018-04-04 08:13:19 -0600

    Add tests for Node class

--- tests/Node-test.js
+++ tests/Node-test.js
@@ -5,10 +5,22 @@ describe('Node', () => {
   let node;
 
   beforeEach(() => {
-    node = new Node('platypi')
-  })
+    node = new Node();
+  });
 
   it('should be a thing', () => {
-    expect(node).to.exist
-  })
+    expect(node).to.exist;
+  });
+
+  it('should have data default to null', () => {
+    expect(node.data).to.equal(null);
+  });
+
+  it('should have end start out false by default', () => {
+    expect(node.end).to.equal(false);
+  });
+
+  it('should have child start as an empty object by default', () => {
+    expect(node.children).to.deep.equal({});
+  });
 })
