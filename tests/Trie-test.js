a4810f255559c67964ef8cb4cad2ee9c8047b704
Ecksi <jimmy.dechant@gmail.com>
2018-04-04 09:49:10 -0600

    Improve tests for insert function

--- tests/Trie-test.js
+++ tests/Trie-test.js
@@ -11,27 +11,53 @@ describe('TRIE', () => {
 
   describe('INSERT', () => {
     it('should add a word to the trie', () => {
-      trie.insert('hitmonchan');
+      trie.insert('magikarp');
 
       expect(trie.count).to.equal(1);
-      expect(trie.root.children.h.data).to.equal('h');
-      expect(trie.root.children.h.children.i.data).to.equal('i');
+      expect(trie.root.children.m.data).to.equal('m');
+      expect(trie.root.children.m.children.a.data).to.equal('a');
+    })
+
+    it('should return undefined if the children node does not exist', () => {
+      trie.insert('allakhazam')
+
+      expect(trie.root.children.a.data).to.equal('a');
+      expect(trie.root.children.a.children.z).to.equal(undefined);
+    })
+
+    it('should have end return true at the end of the word', () => {
+      trie.insert('mew');
+
+      expect(trie.root.children.m.end).to.equal(false);
+      expect(trie.root.children.m.children.e.end).to.equal(false);
+      expect(trie.root.children.m.children.e.children.w.end).to.equal(true);
+    })
+
+    it('should have multiple keys when the same children Node is used', () => {
+      trie.insert('hitmonchan');
+      trie.insert('hatmonlee');
+
+      expect(Object.keys(trie.root.children.h.children).length).to.equal(2);
     })
 
     it('should be able to add four words to the trie', () => {
-      trie.insert('hitmonchan');
-      trie.insert('hatmonlee');
-      trie.insert('mew');
-      trie.insert('bulbasaur');
-      trie.insert('diglet');
+      trie.insert('weedle');
+      trie.insert('pidgey');
+      trie.insert('rattata');
+      trie.insert('caterpie');
 
-      expect(trie.count).to.equal(5);
-      expect(Object.keys(trie.root.children.h.children).length).to.equal(2);
-      expect(trie.root.children.m.children.e.children.w.end).to.equal(true);
-      expect(trie.root.children.b.data).to.equal('b');
-      expect(trie.root.children.b.children.i).to.equal(undefined);
-      expect(trie.root.children.d.data).to.equal('d');
-      expect(trie.root.children.d.children.i.data).to.equal('i');
+      expect(trie.count).to.equal(4);
+      expect(trie.root.children.c.data).to.equal('c');
+      expect(trie.root.children.c.children.a.data).to.equal('a');
+    })
+
+    it('should not count duplicate words', () => {
+      trie.insert('machop');
+      
+      expect(trie.count).to.eq(1);
+      trie.insert('machop');
+
+      expect(trie.count).to.eq(1);
     })
   })
 })
